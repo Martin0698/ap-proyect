3ecc72e60f291e60fa4df83f3d15d0734a3e1e84
Luis Angel Garcia Mendez <luis_7gm@outlook.com>
2021-11-14 11:24:24 -0600

    Cbreak mode. simpleansi is not working

--- Pacman.go
+++ Pacman.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
 )
 
 //READ THE MAZE DATA
@@ -39,6 +40,7 @@ func loadMaze(file string) error {
 //2.  PRINTING TO THE TERMINAL
 
 func printMaze() {
+	//simpleansi.ClearScreen() //we need to clear the screen after each loop
 	for _, line := range maze {
 		fmt.Println(line)
 	}
@@ -47,6 +49,10 @@ func printMaze() {
 //3. GAME LOOP
 
 func main() {
+
+	cbreakMode()
+	defer cookedMode()
+
 	err := loadMaze("maze01.txt")
 	if err != nil {
 		log.Println("failed to load maze: ", err)
@@ -54,6 +60,60 @@ func main() {
 	}
 	for {
 		printMaze()
-		break
+
+		input, err := readInput()
+		if err != nil {
+			log.Println("Error reading input: ", err)
+			break
+		}
+
+		if input == "ESC" {
+			break
+		}
 	}
+}
+
+//4. TERMINAL MODE -> CBREAK MODE
+
+/*
+ cbreak mode somo characters are preprocessed and
+ some are not. This terminal mode to allow us
+ to handle the keys "esc" and arrow keys
+*/
+
+func cbreakMode() {
+	cbTerm := exec.Command("stty", "cbreak", "echo")
+	cbTerm.Stdin = os.Stdin
+
+	err := cbTerm.Run()
+	if err != nil {
+		log.Fatalln("Unable to activate cbreak mode: ", err)
+	}
+}
+
+//RESTORING COOKED MODE
+
+func cookedMode() {
+	cookedTerm := exec.Command("stty", "-cbreak", "echo")
+	cookedTerm.Stdin = os.Stdin
+
+	err := cookedTerm.Run()
+	if err != nil {
+		log.Fatalln("Unable to restore cooked mode: ", err)
+	}
+}
+
+//Reading from StdIn
+
+func readInput() (string, error) {
+	buffer := make([]byte, 100)
+
+	cnt, err := os.Stdin.Read(buffer)
+	if err != nil {
+		return "", err
+	}
+	if cnt == 1 && buffer[0] == 0x1b { //Ox1b represents ESCAPE key
+		return "ESC", nil
+	}
+	return "", nil
 }
